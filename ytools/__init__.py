24cb763101f7a2b84ad03e4ed5cbea8400fbaddc
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-06-13 19:04:48 +0100

    Fixed license header in __init__.py

--- ytools/__init__.py
+++ ytools/__init__.py
@@ -7,23 +7,8 @@ Library for validating `yaml` files against schema and selectively dumping nodes
 #
 #  Copyright (c) 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 #  Copyright (c) Jakob Stemberger <yaccob@gmx.net>
-
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#
+#  Apache 2.0 Licensed
+#  See LICENSE for more information
 #
 
 __author__ = "Dominic Davis-Foster"
